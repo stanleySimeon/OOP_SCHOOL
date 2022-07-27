8eebd581f77c5a888b39f68f22ad39be290ec5d4
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-07-27 18:24:41 +0100

    Add change

--- decorator.rb
+++ decorator.rb
@@ -3,8 +3,10 @@ require_relative 'Nameable'
 class Decorator < Nameable
     def initialize(nameable)
         @nameable = nameable
+        def correct_name
+            @nameable.correct_name
+        end
     end
-# Implementation of the correct_name method that returns the result of the correct_name method of the @nameable.
     def correct_name
         @nameable.correct_name
     end
